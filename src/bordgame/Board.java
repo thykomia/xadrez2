f5174127c9e42084a0292dbd7552fe972450df81
Thyago <thyagoaraujo@live.com>
2020-04-13 13:48:08 -0300

    Initial Setup king rook

--- src/bordgame/Board.java
+++ src/bordgame/Board.java
@@ -15,7 +15,11 @@ public class Board {
 	public  Piece piece (int row, int column) {
 		return pieces[row][column];
 	}
-
+	public void placePiece (Piece piece, Position position) {
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
+		
+	}
 	public  Piece piece (Position position) {
 		return pieces[position.getRow()][position.getColumn()];
 	}
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package chess;
 
 import bordgame.Board;
+import bordgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 
@@ -8,6 +11,7 @@ public class ChessMatch {
 
 	public ChessMatch() {
 		this.board = new Board(8, 8);
+		initialSetup();
 	} // default
 
 	public ChessPiece[][] getPieces() {
@@ -20,6 +24,12 @@ public class ChessMatch {
 		}
 		
 		return mat;
+	}
+	
+	private void initialSetup() {
+		board.placePiece(new Rook(board,Color.BLACK), new Position(2,1));
+		board.placePiece(new King(board,Color.WHITE), new Position(1,1));
+		board.placePiece(new Rook(board,Color.BLACK), new Position(0,1));
 	}
 
 	public Board getBoard() {
